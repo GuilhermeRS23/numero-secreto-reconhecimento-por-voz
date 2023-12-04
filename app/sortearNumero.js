b0be80626bc07fb3d5d472a9ed65624d5572eef0
GuilhermeRS23 <guilhermemuralha2016@gmail.com>
2023-12-04 20:23:40 -0300

    Aula 2 - Aplicando função Math.randon

---
+++ app/sortearNumero.js
@@ -0,0 +1,15 @@
+const elementoMinValor = document.getElementById('min-valor');
+const elementoMaxValor = document.getElementById('max-valor');
+
+let minValor = 1;
+let maxValor = 550;
+const numeroSecreto = gerarNumeroSecreto()
+
+function gerarNumeroSecreto() {
+    return parseInt(Math.random() * maxValor + 1)
+}
+
+console.log("Numero Secreto: " + numeroSecreto)
+
+elementoMaxValor.innerHTML  = maxValor;
+elementoMinValor.innerHTML = minValor;
